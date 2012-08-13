25899a4f4f896eb300361ffafbf8dafbe9c52745
Hans Kindberg <hans.kindberg@gmail.com>
2012-08-13 13:52:27 +0200

    Add to GitHub

---
+++ ProjectTemplates/CSharp/HansKindberg-TestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Tests for XXX")]
+[assembly: Guid("$guid1$")]
+// ReSharper disable CheckNamespace
+internal static class AssemblyInfo
+// ReSharper restore CheckNamespace
+{
+	#region Fields
+
+	internal const string AssemblyName = "$projectname$";
+
+	#endregion
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+HansKindberg-VisualStudio-Templates
+===================================
+Visual Studio "vstemplates".
+
+HansKindberg-EmptySolution
+--------------------------
+Empty solution with "CodeAnalysis" and "Properties" solution folders.
+
+HansKindberg-ClassLibrary
+-------------------------
+
+HansKindberg-TestProject
+------------------------
